c2491df4a871770182c5676280f1498c296eb4e0
Lorenzo-Arnau <skyline03@hotmail.it>
2023-05-14 23:24:52 +0200

    mail funzionanti

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
 
 
-
-
 function copyToSend() {
-
+   document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   var copyText = window.location.href;
   navigator.clipboard.writeText(copyText);
   var modal = document.getElementsByClassName('fake-modal')[0]
@@ -15,6 +14,8 @@ function copyToSend() {
   overlay.classList.add('visible')
 }
 function openTextArea(){
+    document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   var modal = document.getElementsByClassName('send-text')[0]
   modal.classList.remove('invisible')
   modal.classList.add('visible')
@@ -33,6 +34,19 @@ function closeModal(){
 function sendMessage(){
    var textarea = document.getElementsByClassName('text-area-message')[0]
    console.log(textarea.value);
+   Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "limafika1@gmail.com",
+    Password : "058B598A11C24CE81978ADFC60948A2ABBF4",
+    To : 'buffering2021@libero.it',
+    From : "limafika1@gmail.com",
+    Subject : "Email Luigi",
+    Body : textarea.value
+  }).then(
+    message => alert(message)
+  );
+
+
     var modal = document.getElementsByClassName('send-text')[0]
     modal.classList.remove('visible')
     modal.classList.add('invisible')
